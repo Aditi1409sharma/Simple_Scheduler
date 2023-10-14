945d6237e77b34820515f4f6b87cd7d505e548f2
ananya-garg-04 <garg.ananya2018@gmail.com>
2023-10-14 11:25:55 +0530

    Updated simpleShell

--- simpleShell.c
+++ simpleShell.c
@@ -17,7 +17,41 @@ struct CommandHistory
     time_t start_time[MAX_HISTORY_SIZE];
     double duration[MAX_HISTORY_SIZE];
     int pids[MAX_HISTORY_SIZE];
+    int priorities[MAX_HISTORY_SIZE];
 };
+
+// Global variables for process management
+struct CommandHistory history;
+int NCPU = 1; // Number of available CPUs
+int current_priority = 1;
+pid_t running_pids[4]; // Stores the PIDs of currently running processes
+
+void stopProcess(int signo)
+{
+    // Stop the currently running process
+    for (int i = 0; i < NCPU; i++)
+    {
+        if (running_pids[i] > 0)
+        {
+            kill(running_pids[i], SIGSTOP);
+        }
+    }
+}
+
+void timerExpired(int signo)
+{
+    // Implement what happens when a timer expires
+    // Resume the stopped processes
+    for (int i = 0; i < NCPU; i++)
+    {
+        if (running_pids[i] > 0)
+        {
+            kill(running_pids[i], SIGCONT);
+        }
+    }
+    // Re-arm the timer
+    alarm(1);
+}
 
 // Function to execute a command
 void executeCommand(char *command, struct CommandHistory *history)
@@ -67,101 +101,6 @@ void executeCommand(char *command, struct CommandHistory *history)
     }
 }
 
-void executePipeCommands(char *commands, struct CommandHistory *history)
-{
-    char **pipe_commands = NULL;
-    char *token;
-    int num_commands = 0;
-    int command_index = history->count;
-    time(&history->start_time[command_index]);
-
-    token = strtok(commands, "|");
-    while (token != NULL)
-    {
-        char *command = (char *)malloc(strlen(token) + 1);
-        if (command == NULL)
-        {
-            perror("Memory allocation for command failed");
-            exit(1);
-        }
-        strcpy(command, token);
-
-        pipe_commands = (char **)realloc(pipe_commands, (num_commands + 1) * sizeof(char *));
-        if (pipe_commands == NULL)
-        {
-            perror("Memory allocation for pipe_commands failed");
-            exit(1);
-        }
-        pipe_commands[num_commands++] = command;
-        token = strtok(NULL, "|");
-    }
-    pipe_commands[num_commands] = NULL;
-
-    int pipe_fds[MAX_COMMAND_LENGTH - 1][2];
-    pid_t child_pids[MAX_COMMAND_LENGTH];
-    int status;
-
-    for (int i = 0; i < num_commands; i++)
-    {
-        if (i < num_commands - 1)
-        {
-            if (pipe(pipe_fds[i]) == -1)
-            {
-                perror("Pipe creation failed");
-                exit(1);
-            }
-        }
-        child_pids[i] = fork();
-        if (child_pids[i] < 0)
-        {
-            perror("Fork failed");
-            exit(1);
-        }
-        else if (child_pids[i] == 0)
-        {
-            history->pids[history->count] = child_pids[i];
-            if (i > 0)
-            {
-                if (dup2(pipe_fds[i - 1][0], 0) == -1) // Redirect stdin
-                {
-                    perror("Duplication of file descriptor failed");
-                    exit(1);
-                }
-                close(pipe_fds[i - 1][0]);
-            }
-
-            if (i < num_commands - 1)
-            {
-                if (dup2(pipe_fds[i][1], 1) == -1) // Redirect stdout
-                {
-                    perror("Duplication of file descriptor failed");
-                    exit(1);
-                }
-                close(pipe_fds[i][0]);
-            }
-            executeCommand(pipe_commands[i], history);
-
-            exit(0);
-        }
-        else
-        {
-            if (i < num_commands - 1)
-            {
-                close(pipe_fds[i][1]);
-            }
-        }
-    }
-    for (int i = 0; i < (num_commands - 1); i++)
-    {
-        waitpid(child_pids[i], &status, 0);
-        // free(pipe_commands[i]);
-    }
-    // free(pipe_commands);
-    time_t end_time;
-    time(&end_time);
-    history->duration[command_index] = difftime(end_time, history->start_time[command_index]);
-    history->count++;
-}
 // Function to display command history
 void displayHistory(struct CommandHistory history)
 {
@@ -170,6 +109,7 @@ void displayHistory(struct CommandHistory history)
     {
         printf("%d: %s\n", i + 1, history.commands[i]);
         printf("    PID : %d\n", history.pids[i]);
+        printf("    Priority: %d\n", history.priorities[i]);
         printf("    Start Time : %s", ctime(&history.start_time[i]));
         printf("    Duration : %.3lf seconds\n", history.duration[i]);
     }
@@ -224,20 +164,6 @@ int main()
             }
         }
 
-        /*else if (strchr(input, '|'))
-        {
-            executePipeCommands(input, &history);
-
-            if (history.count < MAX_HISTORY_SIZE)
-            {
-                strcpy(history.commands[history.count], input);
-                history.count++;
-            }
-            else
-            {
-                perror("History is full can't add more commands");
-            }
-        }*/
         else
         {
             // Execute the command
